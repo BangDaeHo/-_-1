3b13cd5f130c0bc04662b11d984a729b8d87e8f3
BangDaeHo <hanava1@naver.com>
2020-04-09 13:26:45 +0900

    메인 메소드 작성

---
+++ main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+/**
+ * Write a description of class hi here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class main
+{
+    public static void main(String [] args){
+        int speed = 60;
+        String signal;
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("신호를 입력하세요: ");
+        signal = scanner.next();
+        
+        if(signal.equals("green")){
+            
+        }
+        else if(signal.equals("yellow")){
+            
+        }
+        else{
+            
+        }
+    }
+}
